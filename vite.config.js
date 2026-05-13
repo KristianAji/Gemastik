b274418f68619216f022d45171bc19777469bca1
Kristian <kristiansuseno212@gmail.com>
2026-05-13 22:22:00 +0800

    fix github pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+  base: '/Gemastik/',
+
   plugins: [
     react(),
     VitePWA({
@@ -17,8 +19,8 @@ export default defineConfig({
         display: 'standalone',
         orientation: 'portrait-primary',
         icons: [
-          { src: '/icon-192.png', sizes: '192x192', type: 'image/png' },
-          { src: '/icon-512.png', sizes: '512x512', type: 'image/png' }
+          { src: '/Gemastik/icon-192.png', sizes: '192x192', type: 'image/png' },
+          { src: '/Gemastik/icon-512.png', sizes: '512x512', type: 'image/png' }
         ]
       }
     })
