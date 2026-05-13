13845a2ed9cae5e7eda93e7c6e5c3a8a85173809
Kristian <kristiansuseno212@gmail.com>
2026-05-13 23:28:00 +0800

    semoga jadi

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,9 @@
-import { HashRouter } from 'react-router-dom'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
 
-function App() {
-  return (
-    <HashRouter>
-      {/* routes */}
-    </HashRouter>
-  )
-}
-
-export default App
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
